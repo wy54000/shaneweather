d1b69227a8878cd392aa5c400f951ae53e872ac8
wy54000 <wy54000@163.com>
2017-06-07 21:00:56 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/shaneweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/shaneweather/android/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.shaneweather.android;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
